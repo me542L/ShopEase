82a7e4fa6ea7c6e5fad99d05f394d39164ce01da
s4kr3d-w0r1d <saksham.madan.oct20@gmail.com>
2025-03-01 22:14:34 +0530

    Commit 1

--- src/main/java/com/example/Shopping_Platform/ShoppingPlatformApplication.java
+++ src/main/java/com/example/Shopping_Platform/ShoppingPlatformApplication.java
@@ -2,18 +2,14 @@ package com.example.Shopping_Platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-@EnableJpaRepositories("com.example.Shopping_Platform.repository")
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.example.Shopping_Platform.model") // Scan for entity classes
+@EnableJpaRepositories(basePackages = "com.example.Shopping_Platform.repository") // Scan for repositories
 public class ShoppingPlatformApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(ShoppingPlatformApplication.class, args);
 	}
-
 }
