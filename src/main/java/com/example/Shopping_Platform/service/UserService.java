2b2e0d156e5ed85f34d5f13c41ed2ba4c4ed77a5
me542L <mishrashubhi429@gmail.com>
2025-04-05 15:07:05 +0530

    Improved Sign up feature

--- src/main/java/com/example/Shopping_Platform/service/UserService.java
+++ src/main/java/com/example/Shopping_Platform/service/UserService.java
@@ -15,4 +15,9 @@ public class UserService {
    public Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
+
+     
+   public void save(User user) {
+        userRepository.save(user);
+    }
 }
